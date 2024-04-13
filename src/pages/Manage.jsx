85f628fd128eff8b70b90e510c3bf2637dbb78ec
Anorak2 <adamcoleberry@gmail.com>
2024-04-13 02:36:41 -0500

    basic form structure

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
 const Manage = () => {
+    const [formData, setFormData] = useState({firstName: "", lastName: "", phoneNumber: "", email: "",
+     idNum: "", address: "", city: "", state: "", jobClass: "",  jobRank: "", hoursWorked:"",
+    name:"", message:"" });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`
+        );
+    };
     return(
-    <div className="Manage">
-        <h1> testing </h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
+
+      <label htmlFor="message">Message:</label>
+      <textarea id="message" name="message" value={formData.message} onChange={handleChange}/>
+
+      <button type="submit">Submit</button>
+    </form>
     );
 }
 export default Manage;
