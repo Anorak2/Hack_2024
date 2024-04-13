cf10f61b7f3f9dca0f93f9883c11f78531a15167
Xavanion <barrettbrown2012@gmail.com>
2024-04-13 17:12:01 -0500

    Small format

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Sidebar from "./components/Sidebar.jsx";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Manage from "./pages/Manage.jsx";
 import Main from "./pages/Main.jsx";
-import axios from 'axios';
 
 function App(){
     return (
