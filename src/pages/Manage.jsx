97ac900a7f3422c9dfead0658d805f9db2d12aff
Anorak2 <adamcoleberry@gmail.com>
2024-04-13 03:00:14 -0500

    finished outline for the forms, going to bed lol

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const Manage = () => {
     const [formData, setFormData] = useState({firstName: "", lastName: "", phoneNumber: "", email: "",
-     idNum: "", address: "", city: "", state: "", jobClass: "",  jobRank: "", name:""});
+     idNum: "", address: "", city: "", state: "", jobClass: "",  jobRank: ""});
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -11,34 +11,36 @@ const Manage = () => {
 
     const handleSubmit = (event) => {
         //event.preventDefault();
-        alert(`f: ${formData.firstName},l: ${formData.lastName} , Email: ${formData.email}, Message: ${formData.message}`
+        alert(`fname: ${formData.firstName},lname: ${formData.lastName} , Email: ${formData.email}, pnum: ${formData.phoneNumber}, id ${formData.idNum}, ad: ${formData.address}, city: ${formData.city}, st: ${formData.state}, jc: ${formData.jobClass}, jr: ${formData.jobRank} `
         );
     };
     return(
     <form onSubmit={handleSubmit}>
-      <label htmlFor="firstName">First Name:</label>
-      <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange}/>
-      <label htmlFor="lastName">Last Name:</label>
-      <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange}/>
+        <label htmlFor="firstName">First Name:</label>
+        <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange}/>
+        <label htmlFor="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange}/>
         
-      <label htmlFor="phoneNumber">Phone Number:</label>
-      <input type="text" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input type="text" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
 
-      <label htmlFor="email">Email:</label>
-      <input type="text" id="email" name="email" value={formData.email} onChange={handleChange}/>
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" name="email" value={formData.email} onChange={handleChange}/>
 
-     <label htmlFor="idNum">ID Number:</label>
-      <input type="text" id="idNum" name="idNum" value={formData.idNum} onChange={handleChange}/>
+        <label htmlFor="idNum">ID Number:</label>
+        <input type="text" id="idNum" name="idNum" value={formData.idNum} onChange={handleChange}/>
 
-      <label htmlFor="jobClass">Job Class:</label>
-      <input type="text" id="jobClass" name="jobClass" value={formData.jobClass} onChange={handleChange}/>
-      <label htmlFor="jobRank">Job Rank:</label>
-      <input type="text" id="jobRank" name="jobRank" value={formData.jobRank} onChange={handleChange}/>
+        <label htmlFor="jobClass">Job Class:</label>
+        <input type="text" id="jobClass" name="jobClass" value={formData.jobClass} onChange={handleChange}/>
+        <label htmlFor="jobRank">Job Rank:</label>
+        <input type="text" id="jobRank" name="jobRank" value={formData.jobRank} onChange={handleChange}/>
 
-      <label htmlFor="city">City:</label>
-      <input type="text" id="city" name="city" value={formData.city} onChange={handleChange}/>
-      <label htmlFor="state">State:</label>
-      <input type="text" id="state" name="state" value={formData.state} onChange={handleChange}/>
+        <label htmlFor="address">Address:</label>
+        <input type="text" id="address" name="address" value={formData.address} onChange={handleChange}/>
+        <label htmlFor="city">City:</label>
+        <input type="text" id="city" name="city" value={formData.city} onChange={handleChange}/>
+        <label htmlFor="state">State:</label>
+        <input type="text" id="state" name="state" value={formData.state} onChange={handleChange}/>
 
       <button type="submit">Submit</button>
     </form>
