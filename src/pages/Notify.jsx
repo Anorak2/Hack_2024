fd17356e3a26977a4a4ba9c9e2225ca40572cf34
Anorak2 <adamcoleberry@gmail.com>
2024-04-14 03:33:05 -0500

    merge conflicts

--- src/pages/Notify.jsx
+++ src/pages/Notify.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 // import axios from 'axios';
-||||||| 42cf0d7
-=======
-import React from 'react';
 import StaffCard from '../components/StaffCard';
 
->>>>>>> 11f1e8b00f340df02b74ad75c3559fc1604afcc3
 
 const Main = () => {
 
@@ -20,15 +15,9 @@ const Main = () => {
 
     return(
     <div className="Manage">
-<<<<<<< HEAD
         <button onClick={sendEmail}>hallo
-||||||| 42cf0d7
-        <button>hallo
-=======
         <h1>Notification Panel</h1>
         <StaffCard/>
-        <button>hallo
->>>>>>> 11f1e8b00f340df02b74ad75c3559fc1604afcc3
         </button>
     </div>
     );
