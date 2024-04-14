b50e31c7f0bff22bf9e886cdbf71c08085f7e877
AlexanderPhibbs <157722683+AlexanderPhibbs@users.noreply.github.com>
2024-04-14 08:25:47 -0500

    Added Title

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,7 +3,8 @@ import "../css/main.css";
 const Main = () => {
     return(
     <>
-    <h1 className="welcome"> Welcome! </h1>
+    <h1 className="welcome"> Welcome to Work Weaver! </h1>
+    
     
 
     <img src={'https://hackku.org/images/flower-left.png'} className="flower"></img>
