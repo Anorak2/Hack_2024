11f1e8b00f340df02b74ad75c3559fc1604afcc3
AlexanderPhibbs <157722683+AlexanderPhibbs@users.noreply.github.com>
2024-04-14 03:29:06 -0500

    Image Added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,12 @@ const Main = () => {
     return(
     <div className="Manage">
         <h1> Welcome! </h1>
+    
+
+    <img src={'https://hackku.org/images/flower-left.png'} style={{
+        marginLeft: '300px',
+        width: '300px',
+        height: '300px'}} ></img>
     </div>
     );
 }
